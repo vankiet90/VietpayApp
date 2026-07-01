22e0cdee85a20df43aeaf3b15d9039200dc47ecd
kietvo <vankiet0990@gmail.com>
2026-07-02 02:58:07 +0700

    Register Dio, CurrencyRemoteDatasource, CurrencyRepository

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,5 +1,20 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+
+import '../api/dio_client.dart';
+import '../../features/currency/datasource/remote/currency_remote_datasource.dart';
+import '../../features/currency/repository/currency_repository.dart';
 
 final sl = GetIt.instance;
 
-Future<void> setupLocator() async {}
+Future<void> setupLocator() async {
+  final dioClient = DioClient();
+
+  sl.registerLazySingleton<Dio>(() => dioClient.dio);
+
+  sl.registerLazySingleton<CurrencyRemoteDatasource>(
+    () => CurrencyRemoteDatasource(sl()),
+  );
+
+  sl.registerLazySingleton<CurrencyRepository>(() => CurrencyRepository(sl()));
+}
