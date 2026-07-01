43c371794c3ab49e61094e1c140dee5373f1b5a0
kietvo <vankiet0990@gmail.com>
2026-07-02 01:52:44 +0700

    Configure environment configuration

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppConfig {
+  AppConfig._();
+
+  static String get apiKey => dotenv.env['API_KEY'] ?? '';
+
+  static String get baseUrl => dotenv.env['BASE_URL'] ?? '';
+}
