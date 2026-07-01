de1c3ddd0adb4ce6e6d8b981fd8c2ab6962cd9bd
kietvo <vankiet0990@gmail.com>
2026-07-02 02:01:28 +0700

    Load environment variables

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 import 'app/app.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'core/di/service_locator.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await setupLocator();
 
   runApp(const VietpayApp());
 }
