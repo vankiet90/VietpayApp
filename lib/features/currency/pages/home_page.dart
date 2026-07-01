2afd6e7be038af440fe38831de8e2d10d71a7364
kietvo <vankiet0990@gmail.com>
2026-07-02 02:58:54 +0700

    Test Repository From HomePage

--- lib/features/currency/pages/home_page.dart
+++ lib/features/currency/pages/home_page.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import '../../../core/di/service_locator.dart';
+import '../repository/currency_repository.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    _testApi();
+  }
+
+  Future<void> _testApi() async {
+    final repository = sl<CurrencyRepository>();
+
+    final response = await repository.fetchLatestRates();
+
+    debugPrint("Base: ${response.base}");
+    debugPrint("Date: ${response.date}");
+    debugPrint("Total rates: ${response.rates.length}");
+    debugPrint("USD -> VND: ${response.rates['VND']}");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Currency Converter')),
-      body: const Center(child: Text('Welcome to Vietpay Assessment')),
+      appBar: AppBar(title: const Text("Currency Converter")),
+      body: const Center(child: Text("Sprint 2")),
     );
   }
 }
