991a86bb903ded17a5fe37a403087018fef12985
kietvo <vankiet0990@gmail.com>
2026-07-02 11:03:05 +0700

    Register currency dependencies

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,10 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../features/currency/datasource/local/currency_local_datasource.dart';
 import '../api/dio_client.dart';
 import '../../features/currency/datasource/remote/currency_remote_datasource.dart';
 import '../../features/currency/repository/currency_repository.dart';
 import '../../features/currency/repository/currency_repository_impl.dart';
+import '../database/app_database.dart';
 
 final sl = GetIt.instance;
 
@@ -13,11 +15,16 @@ Future<void> setupLocator() async {
 
   sl.registerLazySingleton<Dio>(() => dioClient.dio);
 
+  sl.registerLazySingleton<AppDatabase>(AppDatabase.new);
+
+  sl.registerLazySingleton<CurrencyLocalDatasource>(
+    () => CurrencyLocalDatasource(sl()),
+  );
   sl.registerLazySingleton<CurrencyRemoteDatasource>(
     () => CurrencyRemoteDatasource(sl()),
   );
 
   sl.registerLazySingleton<CurrencyRepository>(
-    () => CurrencyRepositoryImpl(sl()),
+    () => CurrencyRepositoryImpl(sl(), sl()),
   );
 }
